1065b33a1a0a0d68720fc6bb38977467ae37fb14
vishnunayak5589 <vishnunayak5589@gmail.com>
2023-10-03 22:06:23 +0530

    Update file to add few changes

--- dbfactuly.php
+++ dbfactuly.php
@@ -1,9 +1,9 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
+$mysql_user = "vishnu";
+$mysql_password = "loki";
 $mysql_database = "loginfaculty";
 $prefix = "";
-$db = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
-mysql_select_db($mysql_database, $bd) or die("Could not select database");
+$db = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database, please try again");
+mysql_select_db($mysql_database, $bd) or die("Could not select database, please try again");
 ?>
